2440eed0a20e658863950c9a859ebffdfa13e538
Robert <robert.pakkanen@helsinki.fi>
2019-08-01 23:53:02 +0300

    exercise 8.4

--- books/index.js
+++ books/index.js
@@ -103,7 +103,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -112,11 +112,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => 
+      books.filter(b => b.author === args.author),
     allAuthors: () => authors
   },
   Author: {
-    bookCount: (root) => 
+    bookCount: root => 
       books.filter(book => 
         book.author === root.name
       ).length
