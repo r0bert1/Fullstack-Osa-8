af85a30d5dc009c2f8f21ddd11a9009a23157fda
Robert <robert.pakkanen@helsinki.fi>
2019-08-27 12:25:30 +0300

    exercises 8.17 and 8.18

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
-import { useMutation, useQuery } from 'react-apollo'
+import { useMutation, useQuery, useApolloClient } from 'react-apollo'
 import { gql } from 'apollo-boost'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import BornForm from './components/BornForm'
+import Login from './components/Login'
 
 const ALL_AUTHORS = gql`
 {
@@ -56,8 +57,17 @@ mutation updateBorn($name: String!, $setBornTo: Int!) {
 }
 `
 
+const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password)  {
+      value
+    }
+  }
+`
+
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
 
   const authors = useQuery(ALL_AUTHORS)
   const books = useQuery(ALL_BOOKS)
@@ -67,13 +77,24 @@ const App = () => {
   const [addBook] = useMutation(CREATE_BOOK, {
     refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }]
   })
+  const [login] = useMutation(LOGIN)
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.clearStore()
+    setPage('authors')
+  }
 
   return (
     <div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        {!token && <button onClick={() => setPage('login')}>login</button>}
+        {token && <button onClick={() => setPage('add')}>add book</button>}
+        {token && <button onClick={() => logout()}>logout</button>}
       </div>
 
       <Authors show={page === 'authors'} result={authors} />
@@ -83,6 +104,13 @@ const App = () => {
       <Books show={page === 'books'} result={books} />
 
       <NewBook show={page === 'add'} addBook={addBook} />
+
+      <Login 
+        show={page === 'login'} 
+        login={login} 
+        setToken={(token) => setToken(token)}
+        setPage={setPage}
+      />
 
     </div>
   )
